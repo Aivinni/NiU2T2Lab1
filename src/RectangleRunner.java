7dfb43afaf3a4d66ca81293a45f3da58716d83b7
aivin1 <aivinni1219@gmail.com>
2023-09-28 10:57:53 -0400

    #9

--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -4,6 +4,13 @@ public class RectangleRunner {
         Rectangle plot2 = new Rectangle(125);
         Rectangle plot3 = new Rectangle();
 
+        //Update
+        plot1.setWidth(75);
+        plot2.setWidth(75);
+        plot2.setLength(75);
+        plot3.setWidth(75);
+
+        /*
         System.out.print("The length of plot1 is: ");
         System.out.println(plot1.getLength());
         System.out.print("The width of plot1 is: ");
@@ -24,5 +31,9 @@ public class RectangleRunner {
         System.out.println(plot3.getWidth());
         System.out.print("The area of plot3 is: ");
         System.out.println(plot3.calculateArea());
+        */
+
+        int totalArea = plot1.calculateArea() + plot2.calculateArea() + plot3.calculateArea();
+        System.out.print("These 3 plots require " + totalArea + " square feet of seed");
     }
 }
