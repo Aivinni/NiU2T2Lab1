889214a8004a7dc04fe7295f522d9fe7ac434409
aivin1 <aivinni1219@gmail.com>
2023-09-28 23:55:13 -0400

    Final

--- src/Point.java
+++ src/Point.java
@@ -15,4 +15,43 @@ public class Point {
         x = 0;
         y = 0;
     }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String coordinate() {
+        return ("(" + x + "," + y + ")");
+    }
+
+    public String quadrant() {
+        if (x > 0 && y > 0) {
+            return "I";
+        } else if (x < 0 && y > 0) {
+            return "II";
+        } else if (x < 0 && y < 0) {
+            return "III";
+        } else if (x > 0 && y < 0) {
+            return "IV";
+        } else if (x == 0 && y == 0) {
+            return "Origin";
+        } else if (x == 0) {
+            return "On y-axis";
+        } else if (y == 0) {
+            return "On y-axis";
+        }
+        return null;
+    }
 }
